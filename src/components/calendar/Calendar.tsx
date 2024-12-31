7c2b9be57458d314eafeff0c9023f49c2da5ec0e
CHW_1207 <dilkusha27@gmail.com>
2024-12-31 18:03:34 +0900

    feat: 캘린더 생성 (진행 중)
    - 캘린더 레이아웃 설정
    - 캘린더 날짜 별 색상 변경
    - 날짜 셀 내 감정 아이콘 구현 (진행 중)
    - 날짜 셀 내 일기 작성 아이콘 구현 (진행 중)

--- src/components/calendar/Calendar.tsx
+++ src/components/calendar/Calendar.tsx
@@ -1,14 +1,65 @@
+import { useState } from "react";
+
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
+import '@fullcalendar/common/main.css';
+
+import '../../styles/components/calendar/Calendar.css'
+import EmotionIcon from './EmotionIcon'
+import CreateDiaryButton from "./CreateDiaryButton";
+
 
 const Calendar = () => {
+  const isDiary = true;
+  // const [isDiary, setIsDiary] = useState(false);
+
   return (
     <div className="calendar-container">
       <FullCalendar
         plugins={[ interactionPlugin, dayGridPlugin ]}
-        initialView="dayGridPlugin"
+        initialView="dayGridMonth"
         selectable={true}
+        locale="ko" // 한글 번역 적용
+        dayHeaderContent={(info) => {
+          // 요일 번역, 글씨 색깔 변경
+          const daysInKorean = ["일", "월", "화", "수", "목", "금", "토"]
+          const day = info.date.getDay();
+          const isSunday = day === 0; // if (day === 0) { isSunday = true; } else { isSunday = false; }
+          const isSaturday = day === 6; // if (day === 6) { isSaturday = true; } else { isSaturday = false; }
+          const color = isSunday ? "red" : isSaturday ? "blue" : "black"
+      
+          return (
+            <span style={{ color }}>
+              {daysInKorean[day]}
+            </span>
+          )
+        }}
+        dayCellContent={(info) => {
+          // 날짜 글씨 색깔 변경
+          const day = info.date.getDay();
+          const isSunday = day === 0;
+          const isSaturday = day === 6;
+          const color = isSunday ? "red" : isSaturday? "blue" : "black"
+       
+          return (
+            <div>
+              <span style={{ color }}>
+                {info.date.getDate() /* {info.dayNumberText.replace("일", "")} */ }
+              </span>
+              <div style={{
+                position: "absolute",
+                bottom: "10%",
+                left: "50%",
+                transform: "translateX(-50%)",
+                fontSize: "66%", // 셀 높이의 2/3 크기
+                cursor: "pointer"
+              }}>
+                {isDiary ? <EmotionIcon /> : <CreateDiaryButton />}
+              </div>
+            </div>
+          )
+        }}
       />
     </div>
   );
