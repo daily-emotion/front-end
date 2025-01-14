09901767247824bef251e4b5d1541700a707fad3
wkddnjswl7 <wkddnjswl7@naver.com>
2025-01-14 17:36:08 +0900

    feat : 서버로부터 일기 데이터를 받아와 일기를 보여주는 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import LoginPage from "./pages/LoginPage";
 import MainPage from "./pages/MainPage";
+import DiaryDetail from "./pages/viewDiaryPage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/main" element={<MainPage />} />
+        <Route path="/view" element={<DiaryDetail date={"2025-1-14"} />} />
       </Routes>
     </Router>
   );
