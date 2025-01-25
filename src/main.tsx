7c84fe7f72a94035c018d03ac788b06e6db63f69
wkddnjswl7 <wkddnjswl7@naver.com>
2025-01-25 19:42:28 +0900

    feat : 개발 환경과 배포 환경 분리 feat : 웹 실행 시 콘솔에 전달되는 SERVER_URL 값 확인

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+console.log('VITE_SERVER_URL:', import.meta.env.VITE_SERVER_URL); // 환경 변수 확인
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
