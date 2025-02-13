53bf6232c336836dc885bae0027d9a3bd492ef3d
CHW_1207 <dilkusha27@gmail.com>
2025-02-13 11:20:02 +0900

    refactor: 로그인 알림 제거

--- src/pages/redirectHandler.tsx
+++ src/pages/redirectHandler.tsx
@@ -15,11 +15,11 @@ const RedirectHandler = () => {
     if (accessToken && refreshToken) {
       localStorage.setItem('Authorization', `Bearer ${accessToken}`);
       localStorage.setItem('Refresh Token', `Bearer ${refreshToken}`);
-      alert('로그인 성공');
+      console.log('로그인 성공');
       navigate('/main');
     } else {
       console.error('No Tokens Received');
-      alert('로그인에 실패하였습니다');
+      console.log('로그인에 실패하였습니다');
     }
   });
 
