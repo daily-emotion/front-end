b2e0fbba88c0c6cea57fbca9c20f36ed13e908cf
CHW_1207 <dilkusha27@gmail.com>
2025-02-13 10:56:28 +0900

    refactor: diaryService 내 BASE_URL 적용

--- src/services/diary/DiaryService.tsx
+++ src/services/diary/DiaryService.tsx
@@ -1,7 +1,5 @@
 import axios from 'axios';
-
-// API 기본 URL 설정
-export const API_BASE_URL = 'https://daily-emotion.site/api';
+import { BASE_URL } from '../../configs/apiConfig';
 
 // Diary 타입 정의
 export interface Diary {
@@ -20,7 +18,7 @@ export const DiaryService = {
   // 프로필 조회
   getName: async () => {
     try {
-      const response = await axios.get(`${API_BASE_URL}/user/profile`);
+      const response = await axios.get(`${BASE_URL}/user/profile`);
       return response.data;
     } catch {
       console.log('API : 프로필 조회에 실패하였습니다.');
@@ -39,7 +37,7 @@ export const DiaryService = {
       };
 
       const response = await axios.post(
-        `${API_BASE_URL}/diaries/${date}`,
+        `${BASE_URL}/diaries/${date}`,
         diaryData,
         {
           headers: {
@@ -52,7 +50,7 @@ export const DiaryService = {
       return response.data;
     } catch {
       console.log('API : 일기 생성에 실패하였습니다.');
-      console.log(`${API_BASE_URL}/diaries/${date}`); // URL 확인
+      console.log(`${BASE_URL}/diaries/${date}`); // URL 확인
     }
   },
 
@@ -64,7 +62,7 @@ export const DiaryService = {
       formData.append('file', imageFile); // 파일 객체 추가
 
       const response = await axios.post(
-        `${API_BASE_URL}/diaries/images`,
+        `${BASE_URL}/diaries/images`,
         formData,
         {
           headers: {
@@ -74,7 +72,7 @@ export const DiaryService = {
         }
       );
 
-      return response.data as string;  // 타입을 명식적으로 변환
+      return response.data as string; // 타입을 명식적으로 변환
     } catch {
       console.log('API : 이미지 첨부에 실패하였습니다.');
     }
@@ -83,15 +81,12 @@ export const DiaryService = {
   // 특정 날짜 일기 조회
   getDiaryByDate: async (date: string) => {
     try {
-      const response = await axios.get<Diary>(
-        `${API_BASE_URL}/diaries/${date}`,
-        {
-          headers: {
-            Authorization: token,
-            'Content-Type': 'application/json',
-          },
-        }
-      );
+      const response = await axios.get<Diary>(`${BASE_URL}/diaries/${date}`, {
+        headers: {
+          Authorization: token,
+          'Content-Type': 'application/json',
+        },
+      });
       return response.data;
     } catch {
       console.log('API : 특정 날짜 일기 조회에 실패하였습니다.');
@@ -103,7 +98,7 @@ export const DiaryService = {
     // Partial<Diary> : Diary 타입의 모든 속성을 필수에서 선택으로 변경됨
     try {
       const response = await axios.put(
-        `${API_BASE_URL}/diaries/${date}`,
+        `${BASE_URL}/diaries/${date}`,
         updateDiary,
         {
           headers: {
@@ -122,7 +117,7 @@ export const DiaryService = {
   // 일기 삭제
   deleteDiary: async (date: string) => {
     try {
-      const response = await axios.delete(`${API_BASE_URL}/diaries/${date}`, {
+      const response = await axios.delete(`${BASE_URL}/diaries/${date}`, {
         headers: {
           Authorization: token,
           'Content-Type': 'application/json',
