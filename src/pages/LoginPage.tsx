3a196b2cc005b78a20773c3767a2c437b8b871f1
Soo186 <rlagustn9797@gmail.com>
2025-02-13 10:28:23 +0900

    refactor : href

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,18 +3,18 @@ import '../styles/pages/LoginPage.css';
 
 const LoginPage = () => {
   const handleLoginNaver = () => {
-    window.location.href = `http://localhost:8080/api/oauth2/authorization/naver`;
-    // window.location.href = `https://dailyemotion.site/api/oauth2/authorization/naver`;
+    // window.location.href = `http://localhost:8080/api/oauth2/authorization/naver`;
+    window.location.href = `https://dailyemotion.site/api/oauth2/authorization/naver`;
   };
 
   const handleLoginKakao = () => {
-    window.location.href = `http://localhost:8080/api/oauth2/authorization/kakao`;
-    // window.location.href = `https://dailyemotion.site/api/oauth2/authorization/kakao`;
+    // window.location.href = `http://localhost:8080/api/oauth2/authorization/kakao`;
+    window.location.href = `https://dailyemotion.site/api/oauth2/authorization/kakao`;
   };
 
   const handleLoginGoogle = () => {
-    window.location.href = `http://localhost:8080/api/oauth2/authorization/google`;
-    // window.location.href = `https://dailyemotion.site/api/oauth2/authorization/google`;
+    // window.location.href = `http://localhost:8080/api/oauth2/authorization/google`;
+    window.location.href = `https://dailyemotion.site/api/oauth2/authorization/google`;
   };
 
   return (
