1adc53f60c769ceea27c5b66b08b88728555f51a
CHW_1207 <dilkusha27@gmail.com>
2025-02-13 16:11:15 +0900

    fix: 감정 통계 비율 코드 수정
    - 변수 중복으로 인해 상태 변화가 일어나지 않았을 가능성이 있어 다르게 설정

--- src/components/report/Report.tsx
+++ src/components/report/Report.tsx
@@ -154,9 +154,9 @@ const Report = ({ year, month }: ReportProps) => {
         // const emotionCounts = emotionCountsMockData.emotionCounts;
         // setEmotionCounts(emotionCounts);
 
-        const emotionPercentages = calculateEmotionPercentages(emotionCounts);
+        const emotionPcts = calculateEmotionPercentages(emotionCounts);
+        setEmotionPercentages(emotionPcts);
         console.log('이번 달 작성된 일기 감정의 백분율: ', emotionPercentages);
-        setEmotionPercentages(emotionPercentages);
       } catch (err) {
         console.log(`월별 감정 통계 조회 실패: ${err}`);
         alert(`월별 일기 감정 통계를 조회하는 데 실패했습니다: ${err}`);
