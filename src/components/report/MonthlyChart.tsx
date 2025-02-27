e56d34c3495f49d71d447dfef51a892cba05f75f
CHW_1207 <dilkusha27@gmail.com>
2025-02-28 07:00:11 +0900

    ui: Main Page 내 월 별 통계 ui 적용

--- src/components/report/MonthlyChart.tsx
+++ src/components/report/MonthlyChart.tsx
@@ -4,6 +4,55 @@ import { useNavigate } from 'react-router-dom';
 import { useCalendarStore } from '../../stores/useCalendarStore';
 import { BASE_URL } from '../../configs/apiConfig';
 
+import * as Recharts from 'recharts';
+const { PieChart, Pie, Cell, ResponsiveContainer } = Recharts;
+
+import stylesDummyMonthlyChart from '../../styles/pages/DummyMonthlyChart.module.css';
+
+import EmotionStatsHeaderIconImage from '../../assets/images/ReportPage/ReportPage_EmotionStats_Header_Icon.png';
+
+import HAPPNESSEMOJI from '../../assets/images/emotions/HAPPYNESS.png';
+import ANGEREMOJI from '../../assets/images/emotions/ANGER.png';
+import DISGUSTEMOJI from '../../assets/images/emotions/DISGUST.png';
+import FEAREMOJI from '../../assets/images/emotions/FEAR.png';
+import INTERESTEMOJI from '../../assets/images/emotions/INTEREST.png';
+import SADNESSEMOJI from '../../assets/images/emotions/SADNESS.png';
+import SHAMEEMOJI from '../../assets/images/emotions/SHAME.png';
+import SURPRISEEMOJI from '../../assets/images/emotions/SURPRISE.png';
+
+const emotionTranslations: Record<string, string> = {
+  HAPPINESS: '행복한',
+  SADNESS: '슬픈',
+  ANGER: '화난',
+  DISGUST: '혐오스러운',
+  FEAR: '무서운',
+  SURPRISE: '놀라운',
+  INTEREST: '궁금한',
+  SHAME: '수치스러운',
+};
+
+const emotionColors: Record<string, string> = {
+  HAPPINESS: '#FD1F9B', // 핑크 (행복한)
+  SADNESS: '#4c4c6d', // 회색 (슬픈)
+  ANGER: '#ff1a1a', // 빨강 (분노)
+  FEAR: '#6600cc', // 보라 (무서운)
+  SURPRISE: '#ff9900', // 주황 (놀란)
+  INTEREST: '#ffcc00', // 노랑 (궁금한)
+  DISGUST: '#00b300', // 초록 (혐오스러운)
+  SHAME: '#0080ff', // 파랑 (수치스러운)
+};
+
+const emotionIcons: Record<string, string> = {
+  HAPPINESS: HAPPNESSEMOJI,
+  SADNESS: SADNESSEMOJI,
+  ANGER: ANGEREMOJI,
+  DISGUST: DISGUSTEMOJI,
+  FEAR: FEAREMOJI,
+  SURPRISE: SURPRISEEMOJI,
+  INTEREST: INTERESTEMOJI,
+  SHAME: SHAMEEMOJI,
+};
+
 const MonthlyChart = () => {
   const navigate = useNavigate();
 
@@ -31,8 +80,32 @@ const MonthlyChart = () => {
   const [emotionPercentages, setEmotionPercentages] = useState<
     Record<string, number>
   >({});
-
+  const [isReady, setIsReady] = useState(false);
   const accessToken = localStorage.getItem('Authorization');
+
+  const pieData =
+    Object.entries(emotionCounts).map(([key, value]) => ({
+      name: key,
+      value, // value: value → value (단축 속성 사용)
+    })) || [];
+
+  const lightenRGBA = (hex: string, alpha: number) => {
+    let r = parseInt(hex.substring(1, 3), 16);
+    let g = parseInt(hex.substring(3, 5), 16);
+    let b = parseInt(hex.substring(5, 7), 16);
+
+    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+  };
+
+  const maxValue = Math.max(...(pieData.map((d) => d.value) || [])); // 최대 value 찾기
+  console.log(`maxValue: ${maxValue}`);
+
+  useEffect(() => {
+    if (emotionCounts && emotionPercentages) {
+      setTimeout(() => setIsReady(true), 0);
+      console.log(`차트에 데이터 전달 완료`);
+    }
+  }, [emotionCounts, emotionPercentages]);
 
   useEffect(() => {
     if (calendarApi) {
@@ -42,26 +115,26 @@ const MonthlyChart = () => {
     }
   }, [calendarApi]);
 
-  useEffect(() => {
-    if (!accessToken) {
-      alert('로그인 상태가 아닙니다. 로그인 후 이용해주세요.');
-      navigate('/');
-    } else {
-      axios
-        .get<{ name: string }>(`${BASE_URL}/user/profile`, {
-          headers: { Authorization: accessToken },
-        })
-        .then((res) => {
-          console.log('사용자 정보 (MonthlyChart): ', res);
-          setUserName(res.data.name);
-        })
-        .catch((err) => {
-          setUserName('Unknown');
-          console.log(`사용자 정보 조회 실패: ${err}`);
-          alert(`사용자 정보를 불러오지 못했습니다: ${err}`);
-        });
-    }
-  }, [accessToken, navigate]);
+  // useEffect(() => {
+  //   if (!accessToken) {
+  //     alert('로그인 상태가 아닙니다. 로그인 후 이용해주세요.');
+  //     navigate('/');
+  //   } else {
+  //     axios
+  //       .get<{ name: string }>(`${BASE_URL}/user/profile`, {
+  //         headers: { Authorization: accessToken },
+  //       })
+  //       .then((res) => {
+  //         console.log('사용자 정보 (MonthlyChart): ', res);
+  //         setUserName(res.data.name);
+  //       })
+  //       .catch((err) => {
+  //         setUserName('Unknown');
+  //         console.log(`사용자 정보 조회 실패: ${err}`);
+  //         alert(`사용자 정보를 불러오지 못했습니다: ${err}`);
+  //       });
+  //   }
+  // }, [accessToken, navigate]);
 
   useEffect(() => {
     const calculateEmotionPercentages = (
@@ -86,22 +159,33 @@ const MonthlyChart = () => {
 
     const fetchEmotionCountsData = async () => {
       try {
-        const res = await axios.get<{
-          yearMonth: string;
-          emotionCounts: Record<string, number>;
-        }>(
-          `${BASE_URL}/reports/emotions/${year}/${month}`,
-          // `https://dailyemotion.site/api/reports/emotions/${year}/${month}`,
-          {
-            headers: { Authorization: accessToken },
-          }
-        );
+        // const res = await axios.get<{
+        //   yearMonth: string;
+        //   emotionCounts: Record<string, number>;
+        // }>(
+        //   `${BASE_URL}/reports/emotions/${year}/${month}`,
+        //   // `https://dailyemotion.site/api/reports/emotions/${year}/${month}`,
+        //   {
+        //     headers: { Authorization: accessToken },
+        //   }
+        // );
 
-        console.log('이번 달 작성된 일기 감정 빈도: ', res.data);
-        setYearMonth(res.data.yearMonth);
-        const emotionCounts = res.data.emotionCounts;
-        // const emotionCounts = emotionCountsMockData.emotionCounts;
-        setEmotionCounts(emotionCounts);
+        // console.log('이번 달 작성된 일기 감정 빈도: ', res.data);
+        // setYearMonth(res.data.yearMonth);
+        // const emotionCounts = res.data.emotionCounts;
+        // // const emotionCounts = emotionCountsMockData.emotionCounts;
+
+        const dummyEmotionCounts = {
+          FEAR: 1,
+          HAPPINESS: 3,
+          SADNESS: 2,
+          INTEREST: 1,
+          ANGER: 1,
+          SURPRISE: 1,
+        };
+
+        setEmotionCounts(dummyEmotionCounts);
+        console.log('이번 달 작성된 일기 감정 빈도: ', emotionCounts);
 
         const emotionPercentages = calculateEmotionPercentages(emotionCounts);
         console.log('이번 달 작성된 일기 감정의 백분율: ', emotionPercentages);
@@ -112,49 +196,207 @@ const MonthlyChart = () => {
     };
 
     fetchEmotionCountsData();
-  }, [month]);
-
-  const emotionTranslations: Record<string, string> = {
-    HAPPINESS: '행복한',
-    SADNESS: '슬픈',
-    ANGER: '화난',
-    DISGUST: '혐오스러운',
-    FEAR: '무서운',
-    SURPRISE: '놀라운',
-    INTEREST: '궁금한',
-    SHAME: '수치스러운',
-  };
-
-  const emotions = Object.keys(emotionTranslations);
-  console.log('감정 한글화: ', emotions);
+  }, [year, month]);
 
   const sortedEmotions = Object.entries(emotionPercentages).sort(
     ([, a], [, b]) => b - a
   );
 
   return (
-    <div className="chart-container">
-      <div className="userName-container">
-        <h2>{userName}님의 이번달은?</h2>
-      </div>
-      <div>
-        <div>{yearMonth}</div>
-        <h3>가장 많이 느꼈던 감정</h3>
-        {sortedEmotions.map(([key, value]) => (
-          <div key={key}>
-            {emotionTranslations[key] || key} ({value || 0}%)
+    <>
+      <div className={stylesDummyMonthlyChart.emotionChartContainer}>
+        <div className={stylesDummyMonthlyChart.emotionChartHeader}>
+          <div className={stylesDummyMonthlyChart.headerIcon}>
+            <span className={stylesDummyMonthlyChart.iconBackground}></span>{' '}
+            {/* 배경을 나타내는 span */}
+            <img
+              className={stylesDummyMonthlyChart.iconImage}
+              src={EmotionStatsHeaderIconImage}
+              alt="감정 통계 페이지 헤더 아이콘 그림"
+            />
           </div>
-        ))}
-      </div>
-      <div>
-        {emotions.map((key) => (
-          <div key={key}>
-            {emotionTranslations[key]} {emotionCounts[key] || 0}일{' '}
-            {emotionPercentages[key] || 0}%
+          <span>{userName}의 이번 달은?</span>
+        </div>
+        <div className={stylesDummyMonthlyChart.chartSection}>
+          <div className={stylesDummyMonthlyChart.chartMain}>
+            <div className={stylesDummyMonthlyChart.chartLeft}>
+              <div className={stylesDummyMonthlyChart.chartHeader}>
+                <p
+                  style={{
+                    margin: '0px',
+                    color: '#1c1f3793',
+                    fontSize: '12px',
+                  }}
+                >
+                  {year}.{String(month).padStart(2, '0')}
+                </p>
+                <h3 style={{ margin: '0px' }}>가장 많이 느꼈던 감정</h3>
+              </div>
+              <div className={stylesDummyMonthlyChart.emotionPercentages}>
+                <div className={stylesDummyMonthlyChart.emotionPercentagesLeft}>
+                  {Object.keys(emotionTranslations)
+                    .slice(0, 4)
+                    .map((key) => (
+                      <div
+                        className={stylesDummyMonthlyChart.emotionPercentage}
+                        key={key}
+                      >
+                        <span
+                          className={stylesDummyMonthlyChart.emotionDot}
+                          style={{
+                            backgroundColor: emotionColors[key],
+                          }}
+                        ></span>
+                        <span>
+                          {emotionTranslations[key]}(
+                          {emotionPercentages[key] || 0}%)
+                        </span>
+                      </div>
+                    ))}
+                </div>
+                <div
+                  className={stylesDummyMonthlyChart.emotionPercentagesRight}
+                >
+                  {Object.keys(emotionTranslations)
+                    .slice(4, 8)
+                    .map((key) => (
+                      <div
+                        className={stylesDummyMonthlyChart.emotionPercentage}
+                        key={key}
+                      >
+                        <span
+                          className={stylesDummyMonthlyChart.emotionDot}
+                          style={{
+                            backgroundColor: emotionColors[key],
+                          }}
+                        ></span>
+                        <span>
+                          {emotionTranslations[key]}(
+                          {emotionPercentages[key] || 0}%)
+                        </span>
+                      </div>
+                    ))}
+                </div>
+              </div>
+            </div>
           </div>
-        ))}
+          <div
+            className={[
+              stylesDummyMonthlyChart.chartRight,
+              stylesDummyMonthlyChart.progressContainer,
+            ].join(' ')}
+          >
+            {isReady ? (
+              <>
+                <ResponsiveContainer width={160} height={160}>
+                  <PieChart>
+                    <Pie
+                      data={pieData} // 차트에 들어갈 데이터
+                      cx="50%" // 차트를 컨테이너 중앙에 배치 (X축)
+                      cy="50%" // 차트를 컨테이너 중앙에 배치 (Y축)
+                      innerRadius={50} // 도넛 차트로 만들기 위한 내부 반지름
+                      outerRadius={70} // 외부 반지름 설정
+                      // dataKey="value" // 데이터에서 'value' 값을 기준으로 크기 설정
+                      dataKey={(entry) => entry.value} // 🚀 함수형 dataKey 사용
+                      stroke="none"
+
+                      // paddingAngle={5} // 각 섹션 사이의 간격 추가
+                    >
+                      {Object.entries(emotionCounts).map(
+                        ([emotion, count], index) => (
+                          <Cell
+                            key={`cell-${index}`}
+                            fill={emotionColors[emotion]}
+                          />
+                        )
+                      )}
+                    </Pie>
+
+                    {/* 바깥쪽 원 */}
+                    <Pie
+                      data={pieData} // 차트에 들어갈 데이터
+                      cx="50%" // 차트를 컨테이너 중앙에 배치 (X축)
+                      cy="50%" // 차트를 컨테이너 중앙에 배치 (Y축)
+                      innerRadius={70} // 도넛 차트로 만들기 위한 내부 반지름
+                      outerRadius={75} // 외부 반지름 설정
+                      dataKey={(entry) => entry.value} // 🚀 함수형 dataKey 사용
+                      stroke="none"
+                      // paddingAngle={5} // 각 섹션 사이의 간격 추가
+                    >
+                      {pieData.map((entry, index) => (
+                        <Cell
+                          key={`cell-${index}`}
+                          fill={
+                            entry.value === maxValue
+                              ? lightenRGBA(emotionColors[entry.name], 0.2)
+                              : 'rgba(0, 0, 0, 0)'
+                          }
+                        />
+                      ))}
+                    </Pie>
+
+                    {/* 안쪽 원 */}
+                    <Pie
+                      data={pieData} // 차트에 들어갈 데이터
+                      cx="50%" // 차트를 컨테이너 중앙에 배치 (X축)
+                      cy="50%" // 차트를 컨테이너 중앙에 배치 (Y축)
+                      innerRadius={45} // 도넛 차트로 만들기 위한 내부 반지름
+                      outerRadius={50} // 외부 반지름 설정
+                      dataKey={(entry) => entry.value} // 🚀 함수형 dataKey 사용
+                      stroke="none"
+                      // paddingAngle={5} // 각 섹션 사이의 간격 추가
+                    >
+                      {pieData.map((entry, index) => (
+                        <Cell
+                          key={`cell-${index}`}
+                          fill={
+                            entry.value === maxValue
+                              ? lightenRGBA(emotionColors[entry.name], 0.2)
+                              : 'rgba(0, 0, 0, 0)'
+                          }
+                        />
+                      ))}
+                    </Pie>
+                  </PieChart>
+                </ResponsiveContainer>
+              </>
+            ) : (
+              <div>로딩 중...</div>
+            )}
+          </div>
+        </div>
+        <div className={stylesDummyMonthlyChart.barCharts}>
+          {Object.keys(emotionTranslations).map((key) => (
+            <div className={stylesDummyMonthlyChart.barChart} key={key}>
+              <img
+                className={stylesDummyMonthlyChart.barChartImg}
+                src={emotionIcons[key]}
+                alt="막대 차트 항목 별 이미지"
+              />
+              <span>{emotionTranslations[key]}</span>
+              {/* <BarChart> */}
+              <div>
+                <strong>{emotionCounts[key] || 0}일</strong>
+              </div>
+              <div
+                className={stylesDummyMonthlyChart.barChartPercentage}
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  backgroundColor: emotionColors[key],
+                  width: '48px',
+                  height: '48px',
+                  alignItems: 'center',
+                  borderRadius: '50%  ',
+                }}
+              >
+                <strong>{emotionPercentages[key] || 0}%</strong>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
